2af53e23f1649152f00b5dee7829c22736dc1312
Danilo Arévalo <jyfy121@gmail.com>
2024-01-10 17:46:39 -0500

    Version JavaScript

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
-import { Resend } from 'resend';
-const resend = new Resend('re_123456789');
+//import { Resend } from 'resend';
+const { Resend } = require('resend');
+
+const resend = new Resend('re_RvEW94Wu_A2QyzErBiS62Mhft2Xbth2FU');
 
 (async function() {
   try {
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['delivered@resend.dev'],
+      to: ['jefersson.arevalo@decimetrix.com'],
       subject: 'Hello World',
       html: '<strong>It works!</strong>'
     });
