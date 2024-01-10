189a807f1b4e0e54d7b4b39613027f1dc49f5438
Danilo Arévalo <55026642+winandtu@users.noreply.github.com>
2024-01-10 17:50:14 -0500

    Update index.js

--- index.js
+++ index.js
@@ -1,13 +1,13 @@
 //import { Resend } from 'resend';
 const { Resend } = require('resend');
 
-const resend = new Resend('re_RvEW94Wu_A2QyzErBiS62Mhft2Xbth2FU');
+const resend = new Resend('');
 
 (async function() {
   try {
     const data = await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
-      to: ['jefersson.arevalo@decimetrix.com'],
+      to: [''],
       subject: 'Hello World',
       html: '<strong>It works!</strong>'
     });
